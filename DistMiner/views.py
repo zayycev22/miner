228915181353aa4760f333e49783918fac830612
zayycev22 <zayycev22@gmail.com>
2021-08-27 03:24:08 +0300

    логин и отображение пользователя

--- DistMiner/views.py
+++ DistMiner/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ExUser
+from django.contrib import auth
+from django.http import HttpResponse, HttpResponseNotFound
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+
+def login_p(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        token = request.POST.get('token')
+        user = auth.authenticate(username=username, password=password, token=token)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/User")
+
+        else:
+            print("Auth problem")
+            return redirect("/")
+
+
+def user_page(request, name):
+    user = ExUser.objects.get(username=name)
+    if user is not None:
+        return render(request, 'user_page.html', {"user_info": user})
+    else:
+        return HttpResponse(status=404)
--- miner/urls.py
+++ miner/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls.static import static
+from django.conf import settings
+from DistMiner.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', index),
+    path('/login', login_p),
+    path('/User', user_page)
 ]
